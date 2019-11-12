3d76a3336ae65611b72b87be6c8ff12c3c880cae
Maarten de Vries <maarten@de-vri.es>
2019-11-13 00:00:06 +0100

    Add tests for assert_match macro.

--- tests/assert.rs
+++ tests/assert.rs
@@ -11,3 +11,27 @@ macro_rules! assert_match {
 		}
 	}}
 }
+
+#[derive(Debug)]
+enum Foo {
+	Foo(i32),
+	Bar(i32),
+}
+
+#[test]
+fn test_assert_match() {
+	assert_match!(Foo::Foo(10) = Foo::Foo(10));
+	assert_match!(Foo::Bar(11) = Foo::Bar(11));
+}
+
+#[test]
+#[should_panic]
+fn test_assert_match_different_variant() {
+	assert_match!(Foo::Foo(10) = Foo::Bar(10));
+}
+
+#[test]
+#[should_panic]
+fn test_assert_match_different_value() {
+	assert_match!(Foo::Foo(10) = Foo::Foo(11));
+}

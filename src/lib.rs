77a8d01b336dbd4713a3302def487e59ec1c90e0
Maarten de Vries <maarten@de-vri.es>
2019-11-12 23:24:42 +0100

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+use nalgebra::{Matrix, DMatrix, Dynamic};
+use numpy::{PyArray, npyffi};
+use pyo3::AsPyPointer;
+
+/// `ndarray` dimension type for two-dimensional arrays.
+type Dim = ndarray::Dim<[usize; 2]>;
+
+/// `nalgebra` buffer type for matrices created by the default allocator.
+type Buffer<N, R, C> = <nalgebra::base::DefaultAllocator as nalgebra::base::allocator::Allocator<N, R, C>>::Buffer;
+
+//fn matrix_to_python<'py>(py: pyo3::Python<'py>, matrix: &MatrixD) -> &'py PyArray<f64, Dim> {
+//	PyArray::from_slice(py, matrix.as_slice())
+//		.reshape(matrix.shape()).unwrap()
+//}
+
+#[derive(Debug)]
+pub enum Error {
+	NotAnArray,
+	Downcast(pyo3::PyDowncastError),
+	WrongDataType, // TODO: add actual and expected data type.
+	WrongDimension, // TODO: add actual and expected dimensions.
+}
+
+pub fn dynamic_matrix_from_python<N>(input: &pyo3::types::PyAny) -> Result<DMatrix<N>, Error>
+where
+	N: nalgebra::Scalar + numpy::types::TypeNum,
+{
+	matrix_from_python_generic(input, Dynamic::new(0), Dynamic::new(0))
+}
+
+pub fn matrix_from_python_generic<'a, N, R, C>(input: &'a pyo3::types::PyAny, rows: R, cols: C) -> Result<nalgebra::Matrix<N, R, C, Buffer<N, R, C>>, Error>
+where
+	N: nalgebra::Scalar + numpy::TypeNum,
+	R: nalgebra::Dim,
+	C: nalgebra::Dim,
+	nalgebra::base::default_allocator::DefaultAllocator: nalgebra::base::allocator::Allocator<N, R, C>,
+{
+	let input = input.downcast_ref::<PyArray<N, Dim>>()?;
+
+	if let Some(rows) = R::try_to_usize() {
+		if rows != input.shape()[0] {
+			return Err(Error::WrongDimension);
+		}
+	}
+
+	if let Some(cols) = C::try_to_usize() {
+		if cols != input.shape()[1] {
+			return Err(Error::WrongDimension);
+		}
+	}
+
+	Ok(nalgebra::Matrix::<N, R, C, Buffer<N, R, C>>::from_iterator_generic(rows, cols, input.as_array().into_iter().map(|x| *x)))
+}
+
+pub fn matrix_from_python<'a, N, R, C>(input: &'a pyo3::types::PyAny) -> Result<nalgebra::Matrix<N, R, C, Buffer<N, R, C>>, Error>
+where
+	N: nalgebra::Scalar + numpy::TypeNum,
+	R: nalgebra::Dim + nalgebra::DimName,
+	C: nalgebra::Dim + nalgebra::DimName,
+	R::Value: std::ops::Mul<C::Value>,
+	<R::Value as std::ops::Mul<C::Value>>::Output: generic_array::ArrayLength<N>,
+{
+	unsafe {
+		if npyffi::array::PyArray_Check(input.as_ptr()) != 1 {
+			return Err(Error::NotAnArray);
+		}
+
+		let input = &mut *(input.as_ptr() as *mut npyffi::objects::PyArrayObject);
+
+		if npyffi::array::PY_ARRAY_API.PyArray_EquivTypenums((*input.descr).type_num, N::typenum_default()) != 1 {
+			return Err(Error::WrongDataType);
+		}
+
+		if input.nd != 2 {
+			return Err(Error::WrongDimension);
+		}
+
+		let input_rows = input.dimensions.add(0).read_volatile() as usize;
+		let input_cols = input.dimensions.add(1).read_volatile() as usize;
+		let row_stride = input.strides.add(0).read_volatile() as usize;
+		let col_stride = input.strides.add(1).read_volatile() as usize;
+
+		let rows = R::try_to_usize().unwrap();
+		let cols = C::try_to_usize().unwrap();
+
+		if input_rows != rows || input_cols != cols {
+			return Err(Error::WrongDimension);
+		}
+
+		let mut output = Matrix::<N, R, C, _>::new_uninitialized();
+
+		for r in 0..rows {
+			for c in 0..cols {
+				output[(r, c)] = *(input.data.add(r * row_stride + c * col_stride) as *const N);
+			}
+		}
+
+		Ok(output)
+	}
+}
+
+impl From<pyo3::PyDowncastError> for Error {
+	fn from(other: pyo3::PyDowncastError) -> Self {
+		Self::Downcast(other)
+	}
+}
---
+++ tests/from_python.rs
@@ -0,0 +1,71 @@
+#![feature(proc_macro_hygiene)]
+
+use inline_python::{python, Context};
+use nalgebra_numpy::{Error, matrix_from_python};
+use nalgebra::U3;
+
+macro_rules! assert_match {
+	( $pat:pat = $expr:expr) => {{
+		let value = $expr;
+		if let $pat = &value {
+			assert!(true)
+		} else {
+			eprintln!("failed to match {} = $expr", stringify!($pat));
+			eprintln!("  with $expr = {}", stringify!($expr));
+			eprintln!("  which evaluates to: {:?}", value);
+			panic!("assertion failed");
+		}
+	}}
+}
+
+#[test]
+fn matrix3_f64() {
+	let gil = pyo3::Python::acquire_gil();
+	let py  = gil.python();
+	let context = Context::new_with_gil(py).unwrap();
+	python! {
+		#![context = &context]
+		import numpy as np
+		matrix = np.array([
+			[1.0, 2.0, 3.0],
+			[4.0, 5.0, 6.0],
+			[7.0, 8.0, 9.0],
+		])
+	}
+
+	let matrix = context.globals(py).get_item("matrix").unwrap();
+
+	assert_eq!(matrix_from_python::<f64, U3, U3>(matrix).unwrap(), nalgebra::Matrix3::new(
+		1.0, 2.0, 3.0,
+		4.0, 5.0, 6.0,
+		7.0, 8.0, 9.0,
+	));
+
+	assert_match!(Err(Error::WrongDataType) = matrix_from_python::<f32, U3, U3>(matrix));
+}
+
+#[test]
+fn matrix3_f32() {
+	let gil = pyo3::Python::acquire_gil();
+	let py  = gil.python();
+	let context = Context::new_with_gil(py).unwrap();
+	python! {
+		#![context = &context]
+		import numpy as np
+		matrix = np.array([
+			[1.0, 2.0, 3.0],
+			[4.0, 5.0, 6.0],
+			[7.0, 8.0, 9.0],
+		]).astype(np.float32)
+	}
+
+	let matrix = context.globals(py).get_item("matrix").unwrap();
+
+	assert_eq!(matrix_from_python::<f32, U3, U3>(matrix).unwrap(), nalgebra::Matrix3::new(
+		1.0, 2.0, 3.0,
+		4.0, 5.0, 6.0,
+		7.0, 8.0, 9.0,
+	));
+
+	assert_match!(Err(Error::WrongDataType) = matrix_from_python::<f64, U3, U3>(matrix));
+}

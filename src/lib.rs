89b2300cbda83628bdd617814d796df46b3ec668
Maarten de Vries <maarten@de-vri.es>
2019-11-13 21:07:48 +0100

    Make matrix_from_python work for all matrices (dynamic, fixed, mixed).

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
-use nalgebra::{Matrix, DMatrix, Dynamic};
+use nalgebra::Matrix;
 use numpy::npyffi::objects::PyArrayObject;
 use numpy::{npyffi};
 use pyo3::AsPyPointer;
-
-/// `ndarray` dimension type for two-dimensional arrays.
-type Dim = ndarray::Dim<[usize; 2]>;
 
 /// `nalgebra` buffer type for matrices created by the default allocator.
 type Buffer<N, R, C> = <nalgebra::base::DefaultAllocator as nalgebra::base::allocator::Allocator<N, R, C>>::Buffer;
@@ -47,46 +44,12 @@ pub struct WrongDataTypeError {
 	pub actual: String,
 }
 
-// pub fn dynamic_matrix_from_python<N>(input: &pyo3::types::PyAny) -> Result<DMatrix<N>, Error>
-// where
-// 	N: nalgebra::Scalar + numpy::types::TypeNum,
-// {
-// 	matrix_from_python_generic(input, Dynamic::new(0), Dynamic::new(0))
-// }
-
-// pub fn matrix_from_python_generic<'a, N, R, C>(input: &'a pyo3::types::PyAny, rows: R, cols: C) -> Result<nalgebra::Matrix<N, R, C, Buffer<N, R, C>>, Error>
-// where
-// 	N: nalgebra::Scalar + numpy::TypeNum,
-// 	R: nalgebra::Dim,
-// 	C: nalgebra::Dim,
-// 	nalgebra::base::default_allocator::DefaultAllocator: nalgebra::base::allocator::Allocator<N, R, C>,
-// {
-// 	let input = input.downcast_ref::<PyArray<N, Dim>>()?;
-
-// 	if let Some(rows) = R::try_to_usize() {
-// 		if rows != input.shape()[0] {
-// 			return Err(Error::WrongShape);
-// 		}
-// 	}
-
-// 	if let Some(cols) = C::try_to_usize() {
-// 		if cols != input.shape()[1] {
-// 			return Err(Error::WrongShape);
-// 		}
-// 	}
-
-// 	// TODO: ndarray and numpy are row major, nalgebra i column major.
-// 	Ok(nalgebra::Matrix::<N, R, C, Buffer<N, R, C>>::from_iterator_generic(rows, cols, input.as_array().into_iter().map(|x| *x)))
-// }
-
-// TODO: See if we can borrow the matrix and create a view.
 pub fn matrix_from_python<'a, N, R, C>(input: &'a pyo3::types::PyAny) -> Result<nalgebra::Matrix<N, R, C, Buffer<N, R, C>>, Error>
 where
 	N: nalgebra::Scalar + numpy::TypeNum,
-	R: nalgebra::Dim + nalgebra::DimName,
-	C: nalgebra::Dim + nalgebra::DimName,
-	R::Value: std::ops::Mul<C::Value>,
-	<R::Value as std::ops::Mul<C::Value>>::Output: generic_array::ArrayLength<N>,
+	R: nalgebra::Dim,
+	C: nalgebra::Dim,
+	nalgebra::base::default_allocator::DefaultAllocator: nalgebra::base::allocator::Allocator<N, R, C>,
 {
 	unsafe { matrix_from_py_object(input.as_ptr()) }
 }
@@ -96,16 +59,15 @@ pub unsafe fn matrix_from_py_object<N, R, C>(
 ) -> Result<nalgebra::Matrix<N, R, C, Buffer<N, R, C>>, Error>
 where
 	N: nalgebra::Scalar + numpy::TypeNum,
-	R: nalgebra::Dim + nalgebra::DimName,
-	C: nalgebra::Dim + nalgebra::DimName,
-	R::Value: std::ops::Mul<C::Value>,
-	<R::Value as std::ops::Mul<C::Value>>::Output: generic_array::ArrayLength<N>,
+	R: nalgebra::Dim,
+	C: nalgebra::Dim,
+	nalgebra::base::default_allocator::DefaultAllocator: nalgebra::base::allocator::Allocator<N, R, C>,
 {
 	let array = cast_to_py_array(array)?;
 	let [input_rows, input_cols] = check_shape::<R, C>(array)?;
 	check_equiv_dtype::<N>(array)?;
 
-	let mut output = Matrix::<N, R, C, _>::new_uninitialized();
+	let mut output = Matrix::<N, R, C, _>::new_uninitialized_generic(R::from_usize(input_rows), C::from_usize(input_cols));
 	copy_data_to_matrix(&mut output, array);
 	Ok(output)
 }
--- tests/from_python.rs
+++ tests/from_python.rs
@@ -1,17 +1,17 @@
 #![feature(proc_macro_hygiene)]
 
 use inline_python::{python, Context};
-use nalgebra_numpy::{Error, matrix_from_python};
-use nalgebra::U3;
+use nalgebra_numpy::matrix_from_python;
+use nalgebra::{Dynamic, U2, U3};
 
 #[macro_use]
 mod assert;
 
+/// Test conversion of a numpy array to a Matrix3<f64>.
 #[test]
 fn matrix3_f64() {
 	let gil = pyo3::Python::acquire_gil();
-	let py  = gil.python();
-	let context = Context::new_with_gil(py).unwrap();
+	let context = Context::new_with_gil(gil.python()).unwrap();
 	python! {
 		#![context = &context]
 		import numpy as np
@@ -22,20 +22,21 @@ fn matrix3_f64() {
 		])
 	}
 
-	let matrix = context.globals(py).get_item("matrix").unwrap();
+	let matrix = context.globals(gil.python()).get_item("matrix").unwrap();
+	let matrix : nalgebra::Matrix3<f64> = matrix_from_python(matrix).unwrap();
 
-	assert_eq!(matrix_from_python::<f64, U3, U3>(matrix).unwrap(), nalgebra::Matrix3::new(
+	assert_eq!(matrix, nalgebra::Matrix3::new(
 		1.0, 2.0, 3.0,
 		4.0, 5.0, 6.0,
 		7.0, 8.0, 9.0,
 	));
 }
 
+/// Test conversion of a numpy array to a Matrix3<f32>.
 #[test]
 fn matrix3_f32() {
 	let gil = pyo3::Python::acquire_gil();
-	let py  = gil.python();
-	let context = Context::new_with_gil(py).unwrap();
+	let context = Context::new_with_gil(gil.python()).unwrap();
 	python! {
 		#![context = &context]
 		import numpy as np
@@ -46,9 +47,134 @@ fn matrix3_f32() {
 		]).astype(np.float32)
 	}
 
-	let matrix = context.globals(py).get_item("matrix").unwrap();
+	let matrix = context.globals(gil.python()).get_item("matrix").unwrap();
+	let matrix : nalgebra::Matrix3<f32> = matrix_from_python(matrix).unwrap();
 
-	assert_eq!(matrix_from_python::<f32, U3, U3>(matrix).unwrap(), nalgebra::Matrix3::new(
+	assert_eq!(matrix, nalgebra::Matrix3::new(
+		1.0, 2.0, 3.0,
+		4.0, 5.0, 6.0,
+		7.0, 8.0, 9.0,
+	));
+}
+
+/// Test conversion of a numpy array to a DMatrix3<f64>.
+#[test]
+fn matrixd() {
+	let gil = pyo3::Python::acquire_gil();
+	let context = Context::new_with_gil(gil.python()).unwrap();
+	python! {
+		#![context = &context]
+		import numpy as np
+		matrix = np.array([
+			[1.0, 2.0, 3.0],
+			[4.0, 5.0, 6.0],
+			[7.0, 8.0, 9.0],
+		])
+	}
+
+	let matrix = context.globals(gil.python()).get_item("matrix").unwrap();
+
+	let matrix : nalgebra::DMatrix<f64> = matrix_from_python(matrix).unwrap();
+	assert_eq!(matrix, nalgebra::DMatrix::from_row_slice(3, 3, &[
+		1.0, 2.0, 3.0,
+		4.0, 5.0, 6.0,
+		7.0, 8.0, 9.0,
+	]));
+}
+
+/// Test conversion of a numpy array to a MatrixMN<f64, Dynamic, U3>.
+#[test]
+fn matrix3d() {
+	let gil = pyo3::Python::acquire_gil();
+	let context = Context::new_with_gil(gil.python()).unwrap();
+	python! {
+		#![context = &context]
+		import numpy as np
+		matrix = np.array([
+			[1.0, 2.0, 3.0],
+			[4.0, 5.0, 6.0],
+			[7.0, 8.0, 9.0],
+		])
+	}
+
+	let matrix = context.globals(gil.python()).get_item("matrix").unwrap();
+
+	let matrix : nalgebra::MatrixMN<f64, Dynamic, U3> = matrix_from_python(matrix).unwrap();
+	assert_eq!(matrix, nalgebra::MatrixMN::<f64, Dynamic, U3>::from_row_slice(&[
+		1.0, 2.0, 3.0,
+		4.0, 5.0, 6.0,
+		7.0, 8.0, 9.0,
+	]));
+}
+
+/// Test conversion of a numpy array to a MatrixMN<f64, U3, Dynamic>.
+#[test]
+fn matrixd3() {
+	let gil = pyo3::Python::acquire_gil();
+	let context = Context::new_with_gil(gil.python()).unwrap();
+	python! {
+		#![context = &context]
+		import numpy as np
+		matrix = np.array([
+			[1.0, 2.0, 3.0],
+			[4.0, 5.0, 6.0],
+			[7.0, 8.0, 9.0],
+		])
+	}
+
+	let matrix = context.globals(gil.python()).get_item("matrix").unwrap();
+
+	let matrix : nalgebra::MatrixMN<f64, U3, Dynamic> = matrix_from_python(matrix).unwrap();
+	assert_eq!(matrix, nalgebra::MatrixMN::<f64, U3, Dynamic>::from_row_slice(&[
+		1.0, 2.0, 3.0,
+		4.0, 5.0, 6.0,
+		7.0, 8.0, 9.0,
+	]));
+}
+
+/// Test conversion of a non-coniguous numpy array.
+#[test]
+fn non_contiguous() {
+	let gil = pyo3::Python::acquire_gil();
+	let context = Context::new_with_gil(gil.python()).unwrap();
+	python! {
+		#![context = &context]
+		import numpy as np
+		matrix = np.array([
+			[1.0, 2.0, 3.0],
+			[4.0, 5.0, 6.0],
+			[7.0, 8.0, 9.0],
+		])[0:2, 0:2];
+	}
+
+	let matrix = context.globals(gil.python()).get_item("matrix").unwrap();
+
+	let matrix : nalgebra::MatrixN<f64, U2> = matrix_from_python(matrix).unwrap();
+	assert_eq!(matrix, nalgebra::MatrixN::<f64, U2>::new(
+		1.0, 2.0,
+		4.0, 5.0,
+	));
+}
+
+/// Test conversion of a column-major numpy array.
+#[test]
+fn column_major() {
+	let gil = pyo3::Python::acquire_gil();
+	let context = Context::new_with_gil(gil.python()).unwrap();
+	python! {
+		#![context = &context]
+		import numpy as np
+		matrix = np.array([
+			[1.0, 2.0, 3.0],
+			[4.0, 5.0, 6.0],
+			[7.0, 8.0, 9.0],
+		], order='F');
+	}
+
+	let matrix = context.globals(gil.python()).get_item("matrix").unwrap();
+
+	let matrix : nalgebra::MatrixN<f64, U3> = matrix_from_python(matrix).unwrap();
+	assert_eq!(matrix, nalgebra::Matrix3::new(
 		1.0, 2.0, 3.0,
 		4.0, 5.0, 6.0,
 		7.0, 8.0, 9.0,

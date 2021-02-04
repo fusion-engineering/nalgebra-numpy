9c903101ea9121bb29eed1a2350b89640c427ad6
Omer BenAmram <omerbenamram@gmail.com>
2021-02-04 19:25:34 +0200

    Update src/from_numpy.rs
    Co-authored-by: Maarten de Vries <maarten@de-vri.es>

--- src/from_numpy.rs
+++ src/from_numpy.rs
@@ -201,7 +201,7 @@ where
 	}
 
 	// Check the data type of the input array.
-	if npyffi::array::PY_ARRAY_API.PyArray_EquivTypenums((*(*array).descr).type_num, t2 as i32) != 1 {
+	if npyffi::array::PY_ARRAY_API.PyArray_EquivTypenums((*(*array).descr).type_num, N::ffi_dtype() as u32 as i32) != 1 {
 		return Err(make_error());
 	}
 
